6f6bf2ecbd99f63361556937bdaece44643fbf0d
Nursultan <nursultan22.11.02@gmail.com>
2020-11-12 12:34:42 +0600

    парсер жк

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+from multiprocessing import Pool
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_all_links(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    tds = soup.find('table', class_='table').find_all('td')
+    links = []
+
+    for td in tds:
+        a = td.find('a').get('href')
+        link = 'http://kenesh.kg' + a
+        links.append(link)
+    return links
+
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    try:
+        name = soup.find('h3', class_='deputy-name').text.strip()
+    except:
+        name = 'Not found!'
+
+    try:
+        number = soup.find('p', class_='mb-10').text.strip()
+    except:
+        number = 'Not found!'
+
+    try:
+        bio = soup.find('div', id='biography').text.strip()
+    except:
+        bio = 'Not found!'
+
+    data = {'name': name, 'number': number, 'bio': bio}
+    return data
+
+
+def write_csv(data):
+    with open('deputy.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow((data['name'], data['number'], data['bio'], 'parsed'))
+
+def make_all(url):
+    html = get_html(url)
+    data = get_page_data(html)
+    write_csv(data)
+
+def main():
+    start = datetime.now()
+    url = "http://www.kenesh.kg/ru/deputy/list/35"
+    all_links = get_all_links(get_html(url))
+    with Pool(40) as p:
+        p.map(make_all, all_links)
+    end = datetime.now()
+    total = end - start
+    print(str(total))
+
+
+if __name__ == '__main__':
+    main()
